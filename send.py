68c0355da1ae74e2ca303df0d10d105adaa7daeb
BiggusMaximus <annastyabagas164@gmail.com>
2023-05-09 16:02:31 +0700

    adding data

--- send.py
+++ send.py
@@ -2,7 +2,7 @@ import socket
 import time
 
 
-esp32_address = ('192.168.18.168', 5005)
+esp32_address = ('192.168.18.068', 5005)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(esp32_address)
 
