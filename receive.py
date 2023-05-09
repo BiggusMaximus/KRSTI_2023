01de9d5fa467c7d8d0197d6d1b37f769d49a76d6
BiggusMaximus <jahekeraton164@gmail.com>
2023-05-09 19:28:30 +0700

    add log

--- receive.py
+++ receive.py
@@ -19,11 +19,15 @@ def callback_esp32_sensor1(client, userdata, msg):
 def client_subscriptions(client):
     client.subscribe('KRSTI/data')
 
+def on_log(client, userdata, level, buf):
+    print("log: ",buf)
+
 
 client = mqtt.Client("rpi_client1") #this should be a unique name
 flag_connected = 0
 
 client.on_connect = on_connect
+client.on_log=on_log
 client.on_disconnect = on_disconnect
 client.message_callback_add('KRSTI/data', callback_esp32_sensor1)
 client.connect('127.0.0.1',1883)
