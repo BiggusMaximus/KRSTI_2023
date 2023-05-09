f467ac14bc5c2ae9697056d0e577f44e3da377a2
BiggusMaximus <annastyabagas164@gmail.com>
2023-05-10 02:25:55 +0700

    to servo beginning

--- PCA9685.py
+++ PCA9685.py
@@ -7,17 +7,10 @@ class ServoPCA9685:
         self.ZeroOffset = ZeroOffset
         self.pwm = Adafruit_PCA9685.PCA9685(address=0x40)
         self.pwm.set_pwm_freq(int(60))
-        self.current_pos = 90
 
-    def move(self, pos, speed=1.0):
-        current_pos = self.current_pos
-        step = 1 if pos > current_pos else -1
-        delay = 0.01 / speed
-        for i in range(current_pos, pos, step):
-            pulse = int((650 - 150) / 180 * i + 150 + self.ZeroOffset)
-            self.pwm.set_pwm(self.Channel, 0, pulse)
-            self.current_pos = i  # Update current position
-            time.sleep(delay)
+    def move(self, pos):
+        pulse = int((650-150)/180*pos+150+self.ZeroOffset)
+        self.pwm.set_pwm(self.Channel, 0, pulse)
 
     def reset(self):
         self.move(int(90))
