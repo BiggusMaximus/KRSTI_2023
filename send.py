f314e01f05521f466b11b41207e6de4ba96dff85
BiggusMaximus <annastyabagas164@gmail.com>
2023-05-09 17:28:50 +0700

    adding mqqt

--- send.py
+++ send.py
@@ -12,22 +12,30 @@ def on_disconnect(client, userdata, rc):
    flag_connected = 0
    print("Disconnected from MQTT server")
    
+# a callback functions 
+def callback_esp32_sensor1(client, userdata, msg):
+    print('ESP sensor1 data: ', msg.payload.decode('utf-8'))
 
 
-def callback(client, userdata, msg):
+def callback_esp32_sensor2(client, userdata, msg):
+    print('ESP sensor2 data: ', str(msg.payload.decode('utf-8')))
+
+def callback_rpi_broadcast(client, userdata, msg):
     print('RPi Broadcast message:  ', str(msg.payload.decode('utf-8')))
 
 def client_subscriptions(client):
-    client.subscribe("krsti/data")
+    client.subscribe("esp32/#")
+    client.subscribe("rpi/broadcast")
 
-client = mqtt.Client("krsti") #this should be a unique name
+client = mqtt.Client("rpi_client1") #this should be a unique name
 flag_connected = 0
 
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
-client.message_callback_add('krsti/data', callback)
+client.message_callback_add('esp32/sensor1', callback_esp32_sensor1)
+client.message_callback_add('esp32/sensor2', callback_esp32_sensor2)
+client.message_callback_add('rpi/broadcast', callback_rpi_broadcast)
 client.connect('127.0.0.1',1883)
-
 # start a new thread
 client.loop_start()
 client_subscriptions(client)
@@ -38,3 +46,4 @@ while True:
     time.sleep(4)
     if (flag_connected != 1):
         print("trying to connect MQTT server..")
+        
