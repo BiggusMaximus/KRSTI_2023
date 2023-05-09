aa6d1cc1b0b27e76893d3dc2884b96c957c257de
BiggusMaximus <annastyabagas164@gmail.com>
2023-05-10 01:19:44 +0700

    finishing the master-slave request and dsp problem

--- send.py
+++ send.py
@@ -1,20 +1,17 @@
 import socket
+import time
 
-ESP32_IP = "192.168.18.168" # replace with the actual IP address of your ESP32
+ESP32_IP = "192.168.18.168"
 ESP32_PORT = 80
 
-message = "ADA"
+def send_request(message):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((ESP32_IP, ESP32_PORT))
+        request = f"GET /message?message={message} HTTP/1.1\r\nHost: {ESP32_IP}\r\n\r\n"
+        sock.sendall(request.encode())
+        response = sock.recv(4096).decode()
+        print(response)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((ESP32_IP, ESP32_PORT))
-
-# Prepare the request string
-
-def sendData(message):
-    request = f"GET /message?message={message} HTTP/1.1\r\nHost: {ESP32_IP}\r\n\r\n"
-    sock.sendall(request.encode())
-    response = sock.recv(4096).decode()
-    print(response)
-    
-
-
+while True:
+    send_request("ITIL")
+    time.sleep(1)
