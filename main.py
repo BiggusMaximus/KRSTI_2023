2ff5e2c1e249172abdd0a24ce1b044d45bdde39a
BiggusMaximus <annastyabagas164@gmail.com>
2023-05-09 10:04:56 +0700

    adding shit

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from audio import *
-from PCA9685 import *
+# from PCA9685 import *
+import time
+import board
+import busio
+import Adafruit_PCA9685
+
+# Initialize I2C bus and PCA9685 module
+i2c = busio.I2C(board.SCL, board.SDA)
+pca = Adafruit_PCA9685.PCA9685(address=0x40)
+
+pca.frequency = 50
+
+def set_servo_angle(channel, angle):
+    pulse_min = 150  
+    pulse_max = 600  
+    pulse_length = int((angle / 180) * (pulse_max - pulse_min) + pulse_min)
+    pca.channels[channel].duty_cycle = pulse_length * 65536 // 4096
 
 
 if __name__ == '__main__':
