236d1e3d6fb251a716b22f91a5fa5d4e5d6683c6
BiggusMaximus <annastyabagas164@gmail.com>
2023-05-09 15:52:05 +0700

    adding data

--- send.py
+++ send.py
@@ -1,19 +1,11 @@
 import socket
 import time
 
-# IP address and port number of the ESP32
-esp32_address = ('192.168.1.100', 5005)
 
-# create a socket object
+esp32_address = ('192.168.18.168', 5005)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# connect to the ESP32
 client_socket.connect(esp32_address)
 
-# send a string and a boolean value to the ESP32
-message = 'Hello, ESP32!'
-value = True
-data = message + ',' + str(value)
-client_socket.send(data.encode())
+
 
 client_socket.close()
