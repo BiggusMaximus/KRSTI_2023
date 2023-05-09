d27bc58af3db2af8d521827ba2bfe554319ab036
BiggusMaximus <annastyabagas164@gmail.com>
2023-05-09 17:38:05 +0700

    adding mqqt

--- KRSTI_2023/src/main.cpp
+++ KRSTI_2023/src/main.cpp
@@ -1,70 +1,151 @@
+
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-// WiFi
-const char *ssid = "mousse";        // Enter your WiFi name
-const char *password = "qweqweqwe"; // Enter WiFi password
+// Replace the SSID/Password details as per your wifi router
+const char *ssid = "Butuh wifi";
+const char *password = "mintamulu";
 
-// MQTT Broker
-const char *mqtt_broker = "broker.emqx.io"; // broker address
-const char *topic = "esp32/test";           // define topic
-const char *mqtt_username = "emqx";         // username for authentication
-const char *mqtt_password = "public";       // password for authentication
-const int mqtt_port = 1883;                 // port of MQTT over TCP
+// Replace your MQTT Broker IP address here:
+const char *mqtt_server = "192.168.18.45";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup()
+long lastMsg = 0;
+
+#define ledPin 2
+
+void blink_led(unsigned int times, unsigned int duration)
 {
-  // Set software serial baud to 115200;
-  Serial.begin(115200);
-  // connecting to a WiFi network
+  for (int i = 0; i < times; i++)
+  {
+    digitalWrite(ledPin, HIGH);
+    delay(duration);
+    digitalWrite(ledPin, LOW);
+    delay(200);
+  }
+}
+
+void setup_wifi()
+{
+  delay(50);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
   WiFi.begin(ssid, password);
+
+  int c = 0;
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
+    blink_led(2, 200); // blink LED twice (for 200ms ON time) to indicate that wifi not connected
+    delay(1000);       //
+    Serial.print(".");
+    c = c + 1;
+    if (c > 10)
+    {
+      ESP.restart(); // restart ESP after 10 seconds
+    }
   }
-  Serial.println("Connected to the WiFi network");
-  // connecting to a mqtt broker
-  client.setServer(mqtt_broker, mqtt_port);
-  client.setCallback(callback);
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void connect_mqttServer()
+{
+  // Loop until we're reconnected
   while (!client.connected())
   {
-    String client_id = "esp32-client-";
-    client_id += String(WiFi.macAddress());
-    Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
-    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
+
+    // first check if connected to wifi
+    if (WiFi.status() != WL_CONNECTED)
     {
-      Serial.println("Public emqx mqtt broker connected");
+      // if not connected, then first connect to wifi
+      setup_wifi();
+    }
+
+    // now attemt to connect to MQTT server
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32_client1"))
+    { // Change the name of client here if multiple ESP32 are connected
+      // attempt successful
+      Serial.println("connected");
+      // Subscribe to topics here
+      client.subscribe("rpi/broadcast");
+      // client.subscribe("rpi/xyz"); //subscribe more topics here
     }
     else
     {
-      Serial.print("failed with state ");
+      // attempt not successful
+      Serial.print("failed, rc=");
       Serial.print(client.state());
+      Serial.println(" trying again in 2 seconds");
+
+      blink_led(3, 200); // blink LED three times (200ms on duration) to show that MQTT server connection attempt failed
+      // Wait 2 seconds before retrying
       delay(2000);
     }
   }
-  // publish and subscribe
-  client.publish(topic, "Hi EMQX I'm ESP32 ^^");
-  client.subscribe(topic);
 }
 
-void callback(char *topic, byte *payload, unsigned int length)
+// this function will be executed whenever there is data available on subscribed topics
+void callback(char *topic, byte *message, unsigned int length)
 {
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
-  Serial.print("Message:");
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
   for (int i = 0; i < length; i++)
   {
-    Serial.print((char)payload[i]);
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
   }
   Serial.println();
-  Serial.println("-----------------------");
+
+  // Check if a message is received on the topic "rpi/broadcast"
+  if (String(topic) == "rpi/broadcast")
+  {
+    if (messageTemp == "10")
+    {
+      Serial.println("Action: blink LED");
+      blink_led(1, 1250); // blink LED once (for 1250ms ON time)
+    }
+  }
+
+  // Similarly add more if statements to check for other subscribed topics
+}
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883); // 1883 is the default port for MQTT server
+  client.setCallback(callback);
 }
 
 void loop()
 {
+
+  if (!client.connected())
+  {
+    connect_mqttServer();
+  }
+
   client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 4000)
+  {
+    lastMsg = now;
+
+    client.publish("esp32/sensor1", "88"); // topic name (to which this ESP32 publishes its data). 88 is the dummy value.
+  }
 }
