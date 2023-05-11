ab323bd9254b229885299f81adb04994b1c54d05
BiggusMaximus <jahekeraton164@gmail.com>
2023-05-11 21:18:45 +0700

    finishing GUI

--- PCA9685.py
+++ PCA9685.py
@@ -8,18 +8,38 @@ def gerakSalam():
     order = [4, 6, 2, 8, 3, 7, 1, 9, 5]
     
     for i in order:        
-		  Servos.servo[i-1].angle = servo_angle[i]
+        Servos.servo[i-1].angle = servo_angle[i]
 
-def gerakCuciTangen():
+def gerakCuciTangan():
     servo_angle = [135, 0, 0, 130, 175, 65, 30, 135, 110]
     order = [4, 6, 2, 8, 3, 7, 1, 9, 5]
 
     for i in order:
-		  Servos.servo[i-1].angle = servo_angle[i]
+        Servos.servo[i-1].angle = servo_angle[i]
 
 def moveReset():
+    servo_angle = [0, 65, 115, 180, 180, 0, 90, 90, 70]
+    order = [4, 6, 2, 8, 3, 7, 1, 9, 5]
+
+    for i in order:
+        Servos.servo[i-1].angle = servo_angle[i]
+
+def gerakMasker():
     servo_angle = [0, 180, 120, 55, 175, 85, 75, 0, 70]
     order = [4, 6, 2, 8, 3, 7, 1, 9, 5]
 
     for i in order:
-		  Servos.servo[i-1].angle = servo_angle[i]
+        Servos.servo[i-1].angle = servo_angle[i]
+
+moveReset()
+time.sleep(1)
+gerakSalam()
+time.sleep(1)
+moveReset()
+time.sleep(1)
+gerakCuciTangan()
+time.sleep(1)
+moveReset()
+time.sleep(1)
+gerakMasker()
+moveReset()
