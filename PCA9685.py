0e40bbf5f674d9c777d09e7d1207955ad0be32bd
BiggusMaximus <jahekeraton164@gmail.com>
2023-05-29 18:36:26 +0700

    finishign

--- PCA9685.py
+++ PCA9685.py
@@ -2,11 +2,11 @@ from adafruit_servokit import ServoKit
 import pandas as pd
 import time
 
-t = 1
+t = 0.5
 T_DELAY = 1
 Servos = ServoKit(channels=16)
 stabil_order = [0, 5, 1, 4, 2, 3, 7, 6, 9, 8, 10, 11, 12, 13, 14, 15]
-salam_order = [2, 3, 0, 5, 1, 4, 7, 6, 9, 8, 10, 11, 12, 13, 14, 15]
+salam_order = [2, 3, 0, 5, 1, 4]
 
 df = pd.read_csv('./data.csv')
 
@@ -17,18 +17,22 @@ def moveOrder(gerakan, order,t):
         time.sleep(t)
 
 def Tari(t):
-    moveOrder("stabil", stabil_order, t)
-    print("salam")
-    moveOrder("salam", salam_order, t)
-    moveOrder("stabil", stabil_order, t)
-    time.sleep(1)
-    print("melambai kiri")
-    moveOrder("melambai_kiri", stabil_order, t)
-    moveOrder("salam", salam_order, t)
-    print("melambai kanan")
-    time.sleep(1)
-    moveOrder("melambai_kanan", stabil_order, t)
-    moveOrder("salam", salam_order, t)
+    for i in range(100):
+        print(i)
+        moveOrder("stabil", stabil_order, t)
+        print("salam")
+        moveOrder("salam", salam_order, t)
+        moveOrder("stabil", salam_order, t)
+        
+        time.sleep(1)
+        print("melambai kiri")
+        moveOrder("melambai_kiri", salam_order, t)
+        moveOrder("salam", salam_order, t)
+        print("melambai kanan")
+        
+        time.sleep(1)
+        moveOrder("melambai_kanan", salam_order, t)
+        moveOrder("salam", salam_order, t)
 
 Tari(t)
 
