8cf27a779fe55581d655c8e77927034b85165abd
BiggusMaximus <annastyabagas164@gmail.com>
2023-05-30 05:46:53 +0700

    adding another folder

--- PCA9685.py
+++ PCA9685.py
@@ -10,71 +10,41 @@ Servos = ServoKit(channels=16)
 slave_servo_index   = [8, 14, 13, 12, 0, 1, 2, 3, 0]
 # Protokol index master
 master_servo_index   = [i for i in range(0,9)]
-# Konstanta kalibrasi 
-k = [-115, -95, -20, -360, -115, -110, -125, 180, -115]
+
 # Index master
 stabil_order        = [0, 5, 1, 4, 2, 3, 7, 6, 9, 8, 10, 11, 12, 13, 14, 15]
 salam_order         = [2, 3, 0, 5, 1, 4]
 
 # Index Slave
 vampire_order       = [0, 1, 2, 3, 8, 12, 13, 14, 15]
-
 df = pd.read_csv('./data.csv')
-
-def convert_index(order):
-    count = 0
-    new_index = []
-    for i in order:
-        for j in slave_servo_index:
-            if i == j:
-                new_index.append(count)
-                count = 0
-                break
-            else:
-                count += 1
-
-            if count == 8:
-                count = 0
 
 
 def moveOrder(gerakan, order, t, slave=False):
-    print(order)
-    order = convert_index(order)
-    print(order)
     for i in order:
-        if i is not None:
-            if slave:
-                angle_after = df[gerakan][master_servo_index[i]] + k[master_servo_index[i]]
-                print(f"slave : {i} => {df[gerakan][master_servo_index[i]]} | master : {master_servo_index[i]} => {df[gerakan][master_servo_index[i]] + k[master_servo_index[i]]}")
-                if not (angle_after < 0):
-                    print("gerak")
-                    Servos.servo[master_servo_index[i]].angle = df[gerakan][master_servo_index[i]] + k[master_servo_index[i]]
-            else:
-                Servos.servo[i].angle = df[gerakan][i]
+        Servos.servo[i].angle = df[gerakan][i]
         time.sleep(t)
 
 def Tari(t):
-    # for i in range(100):
-    #     print(i)
-    #     moveOrder("stabil", stabil_order, t)
-    #     moveOrder("salam", salam_order, t)
-    #     moveOrder("stabil", salam_order, t)
+    for i in range(100):
+        print(i)
+        moveOrder("stabil", stabil_order, t)
+        moveOrder("salam", salam_order, t)
+        moveOrder("stabil", salam_order, t)
         
-    #     time.sleep(1)
-    #     print("melambai kiri")
-    #     moveOrder("melambai_kiri", salam_order, t)
-    #     moveOrder("salam", salam_order, t)
-    #     print("melambai kanan")
+        time.sleep(1)
+        print("melambai kiri")
+        moveOrder("melambai_kiri", salam_order, t)
+        moveOrder("salam", salam_order, t)
+        print("melambai kanan")
         
-    #     time.sleep(1)
-    #     moveOrder("melambai_kanan", salam_order, t)
-    #     moveOrder("salam", salam_order, t)
+        time.sleep(1)
+        moveOrder("melambai_kanan", salam_order, t)
+        moveOrder("salam", salam_order, t)
 
-    #     time.sleep(1)
-    #     moveOrder("vampire", salam_order, t)
-    #     moveOrder("salam", salam_order, t)
-
-    moveOrder("vampire", vampire_order, t, slave=True)
+        time.sleep(1)
+        moveOrder("vampire", salam_order, t)
+        moveOrder("salam", salam_order, t)
 
 
 Tari(t)
